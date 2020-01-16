f939663e079a43b04851bc9db22750f794426852
misha1737 <mikekozlov2@gmail.com>
2020-01-16 22:59:26 +0200

    Signed-off-by: misha1737 <mikekozlov2@gmail.com>

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -2,20 +2,56 @@
 <template>
   <div class="ChatPage">
     <h1>Home</h1>
-   
+    <div class="selectImg" @click="OpenFileSelect()">
+      selectImg
+    </div>
+<form  method="post" enctype="multipart/form-data" id="form-logo-upload">
+      <input
+      class="inputFile"
+      type="file"
+      name="filedata"
+      id="logo-upload"
+      @change="UploadLogo()"
+      accept="image/*"
+    />
+    </form>
   </div>
 </template>
 
 <script>
+import Axios from "axios";
 export default {
-  name: 'ChatPage',
+  name: 'HomePage',
   props: {
     msg: String
+  },
+  methods:{
+    OpenFileSelect(){
+      let el = document.getElementById("logo-upload");
+      if (el) el.click();
+    },
+    UploadLogo(){
+      let el = document.getElementById("form-logo-upload");
+      var formData= new FormData(el);
+         Axios.post("http://kmuauto.ru:5000/uploadImg", formData
+        ).then(res => {
+          console.log(res);  
+        });
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
-
+.selectImg{
+  background-color: yellow;
+  width:200px;
+  height:200px;
+  text-align: center;
+  margin:auto;
+}
+.inputFile{
+  opacity:0.5;
+}
 </style>
--- index.js
+++ index.js
@@ -9,19 +9,14 @@ var cors = require('cors');
 var cookieParser = require('cookie-parser')
 var session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
-
 const bodyParser = require('body-parser');
  app.use(bodyParser.urlencoded({extended:true}))
  app.use(bodyParser.json())
-
  app.use(cors(
 {origin: 'http://kmuauto.ru',
  credentials: true,
 },
-
 ));
-
-
 app.use(cookieParser())
 const routes = require('./routes');
 
@@ -39,6 +34,49 @@ app.use(session({
     })
   }))
 
+  const path = require('path');
+  var multer  = require('multer');
+  var fs = require('fs');
+  var dirname='uploads/upl'
+  const storage = multer.diskStorage({
+      destination:(req,file,cb)=>{
+  
+          try {
+              fs.statSync(dirname);
+              console.log('file or directory exists');
+          }
+          catch (err) {
+            if (err.code === 'ENOENT') {
+              fs.mkdirSync(dirname);
+              console.log('file or directory does not exist');
+            }
+          }
+  
+  
+          cb(null,dirname);
+      },
+      filename:(req,file,cb)=>{
+          console.log(file.originalname);
+          cb(null, Date.now() + path.extname(file.originalname))
+      }
+  });
+  
+  const upload =multer({
+      storage,
+      limits: { fileSize: 10 *1024*1024},
+      fileFilter:(req,file,cb) => {
+          const ext = path.extname(file.originalname);
+          
+          if (ext !== '.jpg' && ext !== '.png' && ext !== '.jpeg'){
+              const err = new Error('Extention');
+              err.code = 'EXTENTION';
+              return cb(err);
+          }
+          cb(null, true);
+      }
+  }).single('filedata');
+
+
   server.listen((process.env.PORT || 5000), function(){
     console.log('listening on *http://kmuauto.ru:5000');
   });
@@ -46,6 +84,21 @@ app.use(session({
 //routes
 app.post('/authorization', routes.authorization);
 app.post('/registration', routes.registration);
+app.post('/uploadImg',  function(req, res, next){
+  upload(req, res, err =>{
+      if (err){
+       if (err.code==='LIMIT_FILE_SIZE'){
+          res.send('Файл дуже великий');
+      }else
+      if(err.code==='EXTENTION'){
+          res.send('невірний формат');
+      }
+  }else{
+      res.send('ok');
+  }
+  })
+
+});
 app.get('/logout', routes.logout);
 app.get('/chat', routes.chat);
 
