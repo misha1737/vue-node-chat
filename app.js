58717450b1e6da709cf750d3434fb6ba4e82575d
misha1737 <mikekozlov2@gmail.com>
2020-01-16 21:48:33 +0200

    new commit

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 //var mongoose = require('../libs/mongoose');
 var Cat = require('./models/cats.js').Cat;
-// var cat= new Cat({
-//     catName:'Misha',
-//     catAge:25
-// }
-// )
 
-// cat.save(function(err, cat, affected){
-//                    console.log(arguments)
-//                  });
 
 Cat.find({}).then(cat=>{
     console.log(cat[0].catAge);
